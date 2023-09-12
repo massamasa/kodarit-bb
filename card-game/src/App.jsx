b8368c5937eb13c2377b7552e5ea8edb9701b537
Matti Österbacka <matti.osterbacka@gmail.com>
2023-09-12 19:18:54 +0300

    viesti

--- card-game/src/App.jsx
+++ card-game/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Card from './Components/Card.jsx'
 
 function App() {
+
+  const pelaajanKortti = {
+    kuva: 'http://placekitten.com/120/100'
+  };
 
   return (
     <>
       <h1>Hello world</h1>
       <div className='card'></div>
+      <Card card={pelaajanKortti}/>
     </>
   )
 }
